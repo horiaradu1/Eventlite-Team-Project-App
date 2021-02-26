16f8e3c8c168fab4b472c60d58f8340f70b5e4ff
Finlay Hudson <finlay.hudson@student.manchester.ac.uk>
2021-02-26 10:28:26 +0000

    Add venueRepository interface

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Component
 @Profile({ "default", "test" })
@@ -32,6 +33,11 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 		}
 
 		// Build and save initial models here.
+//		Venue venue = new Venue();
+//		venue.setName("Our event");
+//		venue.setCapacity(10);
+//		venue.setId(0);
+//		venueService.save(venue);
 
 	}
 }
