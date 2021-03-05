24da7991b230cf69a23441e97fc5354f05ac671a
Matei-Alexandru Costin <matei-alexandru.costin@student.manchester.ac.uk>
2021-03-05 17:14:03 +0000

    Add partial implementation for updating events

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,18 +1,22 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
-import uk.ac.man.cs.eventlite.dao.VenueService;
 
 import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -20,9 +24,6 @@ public class EventsController {
 
 	@Autowired
 	private EventService eventService;
-
-	@Autowired
-	private VenueService venueService;
 
 	@GetMapping
 	public String getAllEvents(Model model) {
@@ -42,5 +43,29 @@ public class EventsController {
 		model.addAttribute("venue", event.getVenue().getName());
 		model.addAttribute("description", event.getDescription());
 		return "events/show";
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String updateEvent(@RequestParam("newid") String id, @RequestParam("newname") String name, 
+			@RequestParam("newdate") String date, @RequestParam("newtime") String time, @RequestParam("newdesc") String description) {
+
+		// Checking if any fields (except description) were left blank
+		if (id == null || name == null || date == null || time == null) {
+			return "redirect:/events";
+		}
+		
+		// Set the event attributes
+		
+		Event event = new Event();
+		event.setId(Long.parseLong(id));
+		event.setName(name);
+		event.setVenueId(1);
+		event.setDate(LocalDate.parse(date));
+		event.setTime(LocalTime.parse(time));
+		//event.setVenue(venue);
+		event.setDescription(description);
+		
+		eventService.save(event);
+		return "redirect:/events";
 	}
 }
