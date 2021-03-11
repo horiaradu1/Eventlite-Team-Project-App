ee06daac69ab8ccf74081f8ac61c86c98d145a39
Matei-Alexandru Costin <matei-alexandru.costin@student.manchester.ac.uk>
2021-03-11 17:03:26 +0000

    Fix structure of updating and add confirmation

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -2,21 +2,26 @@ package uk.ac.man.cs.eventlite.controllers;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Event;
-import uk.ac.man.cs.eventlite.entities.Venue;
+
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -24,6 +29,8 @@ public class EventsController {
 
 	@Autowired
 	private EventService eventService;
+	@Autowired
+	private VenueService venueService;
 
 	@GetMapping
 	public String getAllEvents(Model model) {
@@ -42,46 +49,19 @@ public class EventsController {
 		model.addAttribute("time", event.getTime());
 		model.addAttribute("venue", event.getVenue().getName());
 		model.addAttribute("description", event.getDescription());
-		model.addAttribute("venues", getAllVenues());
+		model.addAttribute("venues", venueService.findAll());
 		return "events/show";
 	}
 	
-	@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public String updateEvent(@RequestParam("newid") String id, @RequestParam("newname") String name, 
-			@RequestParam("newdate") String date, @RequestParam("newtime") String time, 
-			@RequestParam("newvenue") String venueid, @RequestParam("newdesc") String description) {
-	
-		// Checking if any fields (except description or time) were left blank
-		if (id == null || name == null || date == null) {
-			return "redirect:/events";
-		}
-		
-		// Set the event attributes
-		Event event = new Event();
-		event.setId(Long.parseLong(id));
-		event.setName(name);
-		event.setVenueId(Long.parseLong(venueid));
-		event.setDate(LocalDate.parse(date));
-		event.setTime(LocalTime.parse(time));
-		event.setDescription(description.stripLeading());
-
+	@PostMapping("/update")
+	public String updateEvent(@RequestBody @Valid @ModelAttribute Event event, RedirectAttributes redirectAttrs) {
+		// Validation
+		String description = event.getDescription().trim();
+		event.setDescription(description);
+		// Saving the event
 		eventService.save(event);
-		return "redirect:/events";
-	}
-	
-	// Used to get an iterable of all venues without accessing venue service
-	private Iterable<Venue> getAllVenues(){
-		// Getting all events
-		Iterable<Event> events = eventService.findAll();
-		ArrayList<Venue> venues = new ArrayList<Venue>();
-		// Finding each unique venue from the list of events
-		for (Event e : events) {
-			Venue v = e.getVenue();
-			if(!venues.contains(v)) {
-				venues.add(v);
-			}
-		}
-		Iterable<Venue> venueIterable = venues;
-		return venueIterable;
+		redirectAttrs.addFlashAttribute("ok_message", "Event updated.");
+		String redirect = "redirect:/events/" + event.getId();
+		return redirect;
 	}
 }
