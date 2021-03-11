e7135e8d29e8bfb226bc5f5de2c1c01a2229c0d3
Matei-Alexandru Costin <matei-alexandru.costin@student.manchester.ac.uk>
2021-03-11 14:26:30 +0000

    Add dropdown menu for updating venues

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -2,6 +2,7 @@ package uk.ac.man.cs.eventlite.controllers;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
-
 import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
@@ -42,30 +42,46 @@ public class EventsController {
 		model.addAttribute("time", event.getTime());
 		model.addAttribute("venue", event.getVenue().getName());
 		model.addAttribute("description", event.getDescription());
+		model.addAttribute("venues", getAllVenues());
 		return "events/show";
 	}
 	
 	@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public String updateEvent(@RequestParam("newid") String id, @RequestParam("newname") String name, 
-			@RequestParam("newdate") String date, @RequestParam("newtime") String time, @RequestParam("newdesc") String description) {
-
-		// Checking if any fields (except description) were left blank
-		if (id == null || name == null || date == null || time == null) {
+			@RequestParam("newdate") String date, @RequestParam("newtime") String time, 
+			@RequestParam("newvenue") String venueid, @RequestParam("newdesc") String description) {
+	
+		// Checking if any fields (except description or time) were left blank
+		if (id == null || name == null || date == null) {
 			return "redirect:/events";
 		}
 		
 		// Set the event attributes
-		
 		Event event = new Event();
 		event.setId(Long.parseLong(id));
 		event.setName(name);
-		event.setVenueId(1);
+		event.setVenueId(Long.parseLong(venueid));
 		event.setDate(LocalDate.parse(date));
 		event.setTime(LocalTime.parse(time));
-		//event.setVenue(venue);
-		event.setDescription(description);
-		
+		event.setDescription(description.stripLeading());
+
 		eventService.save(event);
 		return "redirect:/events";
+	}
+	
+	// Used to get an iterable of all venues without accessing venue service
+	private Iterable<Venue> getAllVenues(){
+		// Getting all events
+		Iterable<Event> events = eventService.findAll();
+		ArrayList<Venue> venues = new ArrayList<Venue>();
+		// Finding each unique venue from the list of events
+		for (Event e : events) {
+			Venue v = e.getVenue();
+			if(!venues.contains(v)) {
+				venues.add(v);
+			}
+		}
+		Iterable<Venue> venueIterable = venues;
+		return venueIterable;
 	}
 }
