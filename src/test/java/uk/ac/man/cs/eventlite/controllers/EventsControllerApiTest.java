fb4bfc6a547c54cd46b18cfa97bf3a41fc458ce3
Horia Radu <horia.radu@student.manchester.ac.uk>
2021-05-07 02:14:28 +0300

    EventsControllerApi Test and Integration Test done

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApiTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApiTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
@@ -52,20 +53,33 @@ public class EventsControllerApiTest {
 
 	@Test
 	public void getIndexWithEvents() throws Exception {
+		DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HH:mm:ss");
+		
 		Event e = new Event();
 		e.setId(0);
 		e.setName("Event");
-		e.setDate(LocalDate.now());
-		e.setTime(LocalTime.now());
-		//e.setVenue(0);
+		e.setDate(LocalDate.of(2021, 01, 01));
+		e.setTime(LocalTime.of(01, 01, 01));
 		Venue v = new Venue();
 		e.setVenue(v);
+		
+		String eventDateTest = e.getDate().format(formatterDate);
+		String eventTimeTest = e.getTime().format(formatterTime);
+		
 		when(eventService.findAll()).thenReturn(Collections.<Event>singletonList(e));
 
 		mvc.perform(get("/api/events").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(handler().methodName("getAllEvents")).andExpect(jsonPath("$.length()", equalTo(2)))
 				.andExpect(jsonPath("$._links.self.href", endsWith("/api/events")))
-				.andExpect(jsonPath("$._embedded.events.length()", equalTo(1)));
+				.andExpect(jsonPath("$._embedded.events.length()", equalTo(1)))
+				.andExpect(jsonPath("$._embedded.events[0].name", equalTo(e.getName())))
+				.andExpect(jsonPath("$._embedded.events[0].id", equalTo((int) e.getId())))
+				.andExpect(jsonPath("$._embedded.events[0].date", equalTo(eventDateTest)))
+				.andExpect(jsonPath("$._embedded.events[0].time", equalTo(eventTimeTest)))
+				.andExpect(jsonPath("$._embedded.events[0].description", equalTo(e.getDescription())))
+				.andExpect(jsonPath("$._embedded.events[0].venueId", equalTo((int) e.getVenueId())))
+				.andExpect(jsonPath("$._embedded.events[0].venue.id", equalTo((int) e.getVenueId())));
 
 		verify(eventService).findAll();
 	}
