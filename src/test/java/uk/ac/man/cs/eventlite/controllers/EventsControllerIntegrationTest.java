99a0bba1644673d0999ac1c5fe4d240c4266523e
Andrei Bostan <andrei.bostan@student.manchester.ac.uk>
2021-05-06 13:04:59 +0100

    Add tests for get and delete requests

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerIntegrationTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerIntegrationTest.java
@@ -1,5 +1,14 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +20,10 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import uk.ac.man.cs.eventlite.EventLite;
 
@@ -21,18 +33,104 @@ import uk.ac.man.cs.eventlite.EventLite;
 @ActiveProfiles("test")
 public class EventsControllerIntegrationTest extends AbstractTransactionalJUnit4SpringContextTests {
 
+	private static Pattern CSRF = Pattern.compile("(?s).*name=\"_csrf\".*?value=\"([^\"]+).*");
+	private static String CSRF_HEADER = "X-CSRF-TOKEN";
+	private static String SESSION_KEY = "JSESSIONID";
+	
 	@LocalServerPort
 	private int port;
+	
+	private int currentRows;
 
 	private WebTestClient client;
 
 	@BeforeEach
 	public void setup() {
+		currentRows = countRowsInTable("events");
 		client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
 	}
 
 	@Test
 	public void testGetAllEvents() {
-		client.get().uri("/events").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk();
+		client.get().uri("/events").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk().expectBody(String.class).consumeWith(result -> {
+			assertThat(result.getResponseBody(), containsString("Upcoming events:"));
+			assertThat(result.getResponseBody(), containsString("Previous events:"));
+		});
 	}
+	
+	@Test
+	public void testGetEvent() {
+		client.get().uri("/events/5").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk().expectHeader()
+				.contentTypeCompatibleWith(MediaType.TEXT_HTML).expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Code Jam 2022"));
+					//System.out.println(result.getResponseBody());
+				});
+	}
+	
+	@Test
+	public void testGetEventsByName() {
+		
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+		form.add("searchName", "code");
+
+		
+		client.get().uri("/events/search?searchName=code").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk().expectHeader()
+				.contentTypeCompatibleWith(MediaType.TEXT_HTML).expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Code Jam 2022"));
+				});
+	}
+	
+	@Test
+	public void testGetAddEventPageNoUser() {
+		client.get().uri("/events/add").accept(MediaType.TEXT_HTML)
+				.exchange().expectStatus().isOk().expectHeader().contentTypeCompatibleWith(MediaType.TEXT_HTML)
+				.expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Not signed in"));
+				});
+	}
+	
+	@Test
+	public void testGetAddEventPageWithUser() {
+		client.mutate().filter(basicAuthentication("Rob", "Haines")).build().get().uri("/events/add").accept(MediaType.TEXT_HTML)
+				.exchange().expectStatus().isOk().expectHeader().contentTypeCompatibleWith(MediaType.TEXT_HTML)
+				.expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Event name:"));
+					assertThat(result.getResponseBody(), containsString("Event date:"));
+					assertThat(result.getResponseBody(), containsString("Event description:"));
+				});
+	}
+	
+	@Test
+	public void testDeleteEvent() {
+		String[] tokens = login();
+		
+		client.delete().uri("/events/5").accept(MediaType.TEXT_HTML).header(CSRF_HEADER,  tokens[0]).
+				cookie(SESSION_KEY, tokens[1]).exchange().expectStatus().isFound();
+		
+		assertThat(currentRows - 1, equalTo(countRowsInTable("events")));
+	}
+	
+	private String[] login() {
+		String[] tokens = new String[2];
+
+		// Although this doesn't POST the log in form it effectively logs us in.
+		// If we provide the correct credentials here, we get a session ID back which
+		// keeps us logged in.
+		EntityExchangeResult<String> result = client.mutate().filter(basicAuthentication("Rob", "Haines")).build().get()
+				.uri("/").accept(MediaType.TEXT_HTML).exchange().expectBody(String.class).returnResult();
+		tokens[0] = getCsrfToken(result.getResponseBody());
+		tokens[1] = result.getResponseCookies().getFirst(SESSION_KEY).getValue();
+
+		return tokens;
+	}
+
+	private String getCsrfToken(String body) {
+		Matcher matcher = CSRF.matcher(body);
+
+		// matcher.matches() must be called; might as well assert something as well...
+		assertThat(matcher.matches(), equalTo(true));
+
+		return matcher.group(1);
+	}
+	
 }
